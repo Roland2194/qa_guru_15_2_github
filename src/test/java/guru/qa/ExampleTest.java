36d3271bb09866bbe804a84fa183ab67597fe571
egor.dobroskok <egor.dobroskok@simbirsoft.com>
2022-09-14 22:38:01 +0400

    fix logic

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 public class ExampleTest {
     @Test
     void exampleTest() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(2 >= 1);
     }
 
     @Test
     void newExampleTest() {
-        Assertions.assertTrue(4 > 2);
+        Assertions.assertTrue(4 >= 2);
     }
 }
